65c61552d0856053844c0eaed4ac444c57585a17
Gabi Payano <payanogabi@gmail.com>
2020-12-03 12:38:06 -0500

    Going to revisit bootstrap, pretty much basic layout. No more windows for accessibility reasons. Also setting up project blocks so I can start displaying individual project sites

--- wp-content/themes/fuck-master-blaster/page.php
+++ wp-content/themes/fuck-master-blaster/page.php
@@ -5,10 +5,10 @@ get_header();
 
 	while (have_posts()){
 		the_post(); ?>
-<<<<<<< HEAD
+
 		<h2><?php the_title();?></h2>
 		<?php the_content(); ?>
-=======
+
 		
 		<div class="page-links">
 			<h2 class="page-links_title"><a href="#">About</a></h2>
@@ -21,7 +21,7 @@ get_header();
 		<div class="generic-content">
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</div>
->>>>>>> master
+
 
 		<?php }
 
